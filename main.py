6c2e83179cf37a54c0d419032a7195ba9f0f6f5e
cbyzz24 <155403160+cbyzz24@users.noreply.github.com>
2024-07-17 22:20:11 -0600

    Formatting Updates

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ with col1:
 #account section
     accts_title=st.subheader(":green[Cash Account Balances]")
 
-    chase_amount=st.text_input("Chase Bank", placeholder="$0.00")
+    chase_amount=st.text_input("Chase Bank", placeholder="0.00")
 
-    capital_one_amount=st.text_input("Capital One", placeholder="$0.00")
+    capital_one_amount=st.text_input("Capital One", placeholder="0.00")
 
-    initial_cash_total=st.text_input("Total Initial Cash Balance", placeholder="$0.00")
+    initial_cash_total=st.text_input("Total Initial Cash Balance", placeholder="0.00")
     st.container(height=68, border=False)
     st.write("---")
     
     #bills section
     bills_title=st.subheader(":red[Bills]")
-    total_bills=st.text_input("Total Bill Amount", placeholder="$0.00")
+    total_bills=st.text_input("Total Bill Amount", placeholder="0.00")
 
 
 #st.write("---")
@@ -31,12 +31,12 @@ with col2:
     trans2_title=st.text_input("Transaction 2", placeholder="Enter Amount")
     trans3_title=st.text_input("Transaction 3", placeholder="Enter Amount")
     #trans4_title=st.text_input("Transaction 4", placeholder="Enter Amount")
-    pending_trans_total=st.text_input("Total Pending Transactions", placeholder="$0.00")
+    pending_trans_total=st.text_input("Total Pending Transactions", placeholder="0.00")
     st.write("---")
 
     #net cash section
     net_cash_title=st.subheader(":rainbow[Net Cash]")
-    total_net_cash=st.text_input(":blue[Total Net Cash]", placeholder="$0.00")
+    total_net_cash=st.text_input(":blue[Total Net Cash]", placeholder="0.00")
     calc_button=st.button(":green[Calculate Cash]")
 
 
